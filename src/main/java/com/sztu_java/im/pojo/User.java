758797210179397a744fcfd3c0f3ceb0e5d3df87
it21 <Lioxbb@outlook.com>
2023-11-25 15:07:28 +0800

    "feat: add jwt_token“

---
+++ src/main/java/com/sztu_java/im/pojo/User.java
@@ -0,0 +1,54 @@
+package com.sztu_java.im.pojo;
+
+public class User {
+    private long id;
+    private String username;
+    private String password;
+    private String image;
+ 
+    public User() {
+    }
+
+    public User(long id, String username, String password, String image) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.image = image;
+    }
+
+    // get set
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
