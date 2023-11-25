73981be5706f5f0bca9b3418f6cd0129bd3c5c96
it21 <Lioxbb@outlook.com>
2023-11-25 14:57:17 +0800

    feat:add netty

--- src/main/java/com/sztu_java/im/ImApplication.java
+++ src/main/java/com/sztu_java/im/ImApplication.java
@@ -1,13 +1,26 @@
 package com.sztu_java.im;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImApplication {
+public class ImApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImApplication.class, args);
+	}
+   
+	//    加入netty
+	@Autowired
+  	private com.sztu_java.im.netty.CoordinationNettyServer nettyServer;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		// throw new UnsupportedOperationException("Unimplemented method 'run'");
+		nettyServer.start();
 	}
 
 }
